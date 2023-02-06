9b91ae458a7f1a316742094d6a32504b80ed4c77
MalekQumboz <malek.hossam.209@gmail.com>
2023-02-06 06:54:24 +0200

    task two

---
+++ stock_management/stock_management/doctype/unit_of_measure/unit_of_measure.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, mohammed abunada and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Unit of Measure', {
+	// refresh: function(frm) {
+
+	// }
+});
